6b9128a14d475bcc9a1b1ca0712a732eba10a460
MrCosik <cirocki.jakub1@gmail.com>
2023-01-29 09:26:42 +0100

    First task done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from treelib import Tree, Node
-
+# 1)
 tree = Tree()
 tree.create_node("Harry", "h")
 tree.create_node("Jane", "j", parent="h")
@@ -20,10 +20,22 @@ z = tree.get_node("h")
 print(z.tag)
 print(z.is_root())
 
+# b)
 def dupilcate_node_path_check(tree,node):
+    current_node = node
+    duplicates = set()
+    while current_node.identifier != tree.root:
+        parent = tree.parent(current_node.identifier)
+        if node.tag == parent.tag:
+            duplicates.add(parent.tag)
+        current_node = parent
+    if len(duplicates) > 0:
+        return True
+    else:
+        return False
 
-
-
+print('----------------------------------------')
+print('b)')
 x = tree.get_node("h2")
 print(dupilcate_node_path_check(tree, x))
 x = tree.get_node("m")
